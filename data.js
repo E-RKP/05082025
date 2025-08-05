02fc42c89d162a1f12459ae490ea6ef5692622b9
E-RKP <edobackup111@gmail.com>
2025-08-05 10:24:44 +0200

    version 2

---
+++ data.js
@@ -0,0 +1,49 @@
+const utenti = [
+  {
+    id: 1,
+    nome: "Marco",
+    eta: 30,
+    ordini: [
+      { id: 101, prodotto: "Laptop", prezzo: 1200 },
+      { id: 102, prodotto: "Mouse", prezzo: 25 }
+    ]
+  },
+  {
+    id: 2,
+    nome: "Giulia",
+    eta: 25,
+    ordini: [
+      { id: 103, prodotto: "Smartphone", prezzo: 800 },
+      { id: 104, prodotto: "Cover", prezzo: 15 },
+      { id: 105, prodotto: "Auricolari", prezzo: 60 }
+    ]
+  },
+  {
+    id: 3,
+    nome: "Luca",
+    eta: 40,
+    ordini: []
+  }
+
+];
+
+function stampanomi (array){
+  for (i=0; i<array.length;i++){
+    console.log(array[i].nome)
+  }
+}
+stampanomi(utenti);
+
+function etaMedia(array){
+  
+  let etaTot = 0
+  let etamediaa = 0
+  for (i=0; i<array.length;i++){
+    etaTot = etaTot+array[i].eta
+  }
+etamediaa = etaTot/array.length
+return Math.floor(etamediaa);
+}
+
+console.log(etaMedia(utenti))
+
